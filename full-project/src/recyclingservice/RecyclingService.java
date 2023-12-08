2215745deace1f5d478f81cc34948a9b232bd425
Davo0416 <60065631+Davo0416@users.noreply.github.com>
2023-12-08 14:06:23 +0000

    Add files via upload

---
+++ full-project/src/recyclingservice/RecyclingService.java
@@ -0,0 +1,25 @@
+package recyclingservice;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author adamp
+ */
+public class RecyclingService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+        UserQuestion UserQuestionUI = new UserQuestion();      
+        
+        UserQuestionUI.setVisible(true);
+    }
+    
+}
